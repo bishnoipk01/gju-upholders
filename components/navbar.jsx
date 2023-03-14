b1f9e0fdc49a6ac72da798e3782ca8c0daa6b0f3
kumarnitin01 <kn2322000@gmail.com>
2023-03-14 11:39:31 +0530

    changed bg

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -1,6 +1,7 @@
 'use client';
 import { useSession, signOut } from 'next-auth/react';
 
+
 import Image from 'next/image';
 import Link from 'next/link';
 export default function NavBar() {
@@ -8,9 +9,9 @@ export default function NavBar() {
   return (
     <nav className='bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900 container mx-auto'>
       <div className='container flex flex-wrap items-center justify-between mx-auto'>
-        <Link href='https://flowbite.com/' className='flex items-center'>
+        <Link href='/' className='flex items-center'>
           <Image
-            src='https://flowbite.com/docs/images/logo.svg'
+            src='/logo.svg'
             width={40}
             height={40}
             className='h-6 mr-3 sm:h-9'
