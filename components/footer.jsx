e6f7c368e3ca5403d7260fc024286818fd06d6cb
bishnoipk01 <pankajbishnoi2001@gmail.com>
2023-03-28 10:40:34 +0530

    added tags

--- components/footer.jsx
+++ components/footer.jsx
@@ -1,11 +1,11 @@
-import Link from "next/link";
+import Link from 'next/link';
 
 export default function Footer() {
   return (
-    <footer className="bg-gray-200 text-center lg:text-left mt-10">
-      <div className="text-gray-700 text-center p-4">
-        © 2023 Copyright:{" "}
-        <Link className="text-gray-800" href="/">
+    <footer className='bg-gray-200 text-center lg:text-left mt-10 fixed bottom-0 min-w-full'>
+      <div className='text-gray-700 text-center p-4'>
+        © 2023 Copyright:{' '}
+        <Link className='text-gray-800' href='/'>
           Gju Upholders
         </Link>
       </div>
